0f8717c38c00702ebd787efacd2f402cd0f51f6c
huu-tu2k4 <148683133+huu-tu2k4@users.noreply.github.com>
2026-05-25 13:55:41 +0700

    chore: update package.json scripts and dependencies; change handbook model table name

--- src/models/handbook.js
+++ src/models/handbook.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Handbook',
-    tableName: 'Handbooks'
+    tableName: 'handbook_categories'
   });
   return Handbook;
 };
